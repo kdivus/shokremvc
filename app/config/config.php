aabc24daacdedcac4c0d83e2f05972ab471d88ed
Krešimir Divić <kdivus@hotmail.com>
2021-01-21 14:11:23 +0100

    names fix

--- app/config/config.php
+++ app/config/config.php
@@ -2,6 +2,6 @@
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost/traversymvc');
+  define('URLROOT', 'http://localhost/shokremvc');
   // Site Name
   define('SITENAME', 'shokremvc');
